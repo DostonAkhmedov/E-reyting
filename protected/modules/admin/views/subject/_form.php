d15911284e0e160823a5bf6a536bbd7f45036732
Smart <Bekzod5>
2016-03-18 02:02:06 +0500

    Doston Ko`rqani

--- protected/modules/admin/views/subject/_form.php
+++ protected/modules/admin/views/subject/_form.php
@@ -11,7 +11,7 @@
 	// Please note: When you enable ajax validation, make sure the corresponding
 	// controller action is handling ajax validation correctly.
 	// There is a call to performAjaxValidation() commented in generated controller code.
-	// See class documentation of CActiveForm for details on this.
+	// See class documentation of CActiveForm for details on this.hrtjrtjrt
 	'enableAjaxValidation'=>false,
 		)); ?>
 	<p class="note">Fields with <span class="required">*</span> are required.</p>
